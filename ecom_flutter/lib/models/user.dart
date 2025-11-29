60ecdfffc561adfe3d970a7765237aec05ede378
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-29 20:25:28 +0530

    desirializetion added

--- ecom_flutter/lib/models/user.dart
+++ ecom_flutter/lib/models/user.dart
@@ -40,6 +40,25 @@ class User {
   // this method directly encodes the data from the Map  into JSON String
 
   // json.encode function convert a Dart object (such as map or list) into a json string representation , making it suitable for comunitation between different system
-  
+  // encode
   String toJSON() => json.encode(toMap());
+
+  // desirialization convert a Map to a User Object
+  // purpose manipulation and use: Once a data is converted to a User Object it can be easily manipulated withen the application. For example we might want to displat the user's fullname , email, on the UI we want to save data locally
+
+  // the factory constructor takes a Map(usually obtaind form a json object)
+  // and convert it into a user object . if field is not present in the map,
+  // ot default to an empty string
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['_id'] as String? ?? "",
+      fullName: map['fullName'] as String? ?? "",
+      email: map['email'] as String? ?? "",
+      state: map['state'] as String? ?? "",
+      city: map['city'] as String? ?? "",
+      locality: map['locality'] as String? ?? "",
+      password: map['password'] as String? ?? "",
+    );
+  }
 }
