546d86840f66d78475c5242125adf5cfb008bc40
kishanghosh090 <kishanghosh090@gmail.com>
2025-12-01 20:40:35 +0530

    admin sidebar done

---
+++ ecom_admin/lib/views/main_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text("Management"),
+      ),
+      body: const Text("Dashboard"),
+      sideBar: const SideBar(
+        items: [
+          AdminMenuItem(
+            title: 'Vendors',
+            route: "",
+            icon: CupertinoIcons.person_3,
+          ),
+          AdminMenuItem(
+            title: 'Buyers',
+            route: "",
+            icon: CupertinoIcons.person,
+          ),
+          AdminMenuItem(
+            title: 'Orders',
+            route: "",
+            icon: CupertinoIcons.shopping_cart,
+          ),
+          AdminMenuItem(title: 'Categories', route: "", icon: Icons.category),
+          AdminMenuItem(title: 'Upload Banners', route: "", icon: Icons.upload),
+          AdminMenuItem(title: 'Products', route: "", icon: Icons.store),
+        ],
+        selectedRoute: '',
+      ),
+    );
+  }
+}
