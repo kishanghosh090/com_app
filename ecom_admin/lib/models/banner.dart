19fd615e55848eb4ee6f5a3ad263721813cb7677
kishanghosh090 <kishanghosh090@gmail.com>
2025-12-02 22:20:45 +0530

    banner model done

---
+++ ecom_admin/lib/models/banner.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+class BannerModel {
+  final String id;
+  final String image;
+  BannerModel({required this.id, required this.image});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{'id': id, 'image': image};
+  }
+
+  factory BannerModel.fromMap(Map<String, dynamic> map) {
+    return BannerModel(id: map['_id'] as String, image: map['image'] as String);
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BannerModel.fromJson(String source) =>
+      BannerModel.fromMap(json.decode(source));
+}
--- ecom_admin/lib/views/side_bar_screens/upload_banner_screen.dart
+++ ecom_admin/lib/views/side_bar_screens/upload_banner_screen.dart
@@ -1,25 +1,119 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String id = '\bannerscreeen';
   const UploadBannerScreen({super.key});
 
   @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "Banners",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 3,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Banners",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
             ),
           ),
-        ),
-        Divider(color: Colors.green, thickness: 2),
-      ],
+          Divider(color: Colors.green, thickness: 2),
+          Row(
+            spacing: 3,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: _image != null
+                    ? Image.memory(_image, fit: BoxFit.cover)
+                    : Center(child: Text("Category Image")),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // if (_formKey.currentState!.validate()) {
+                  //   await _categoryController.uploadCategory(
+                  //     pickedImage: _image,
+                  //     pickedBanner: _bannerImage,
+                  //     name: categoryName,
+                  //     context: context,
+                  //   );
+                  // }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green.shade200,
+                  textStyle: TextStyle(color: Colors.green),
+                ),
+                child: Text("save"),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                await pickImage();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                elevation: 8,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
+                ),
+                textStyle: const TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                ),
+              ),
+
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.image, size: 15),
+                  SizedBox(width: 8),
+                  Text("Pick Image"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
